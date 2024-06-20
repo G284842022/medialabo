5b2df5c860f293817f3fa4177b5fe39e3f0f7b0a
G284842022 <105191738+G284842022@users.noreply.github.com>
2024-06-20 16:19:44 +0900

    kazuateを作成

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,10 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+for(let e of campus.buildingD) {
+	console.log(e);
+}
+for(let e of gakka) {
+	console.log(e.name);
+}
